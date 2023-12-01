b918aabf4ba3d19d6eae33c8d5f976eea15ff719
Quentin <contact@yovach.dev>
2023-12-01 10:05:44 +0100

    feat: extract only numbers from line

--- day-1/src/main.rs
+++ day-1/src/main.rs
@@ -1,4 +1,21 @@
-fn main() {
+use std::fs;
 
-    println!("Hello, world!");
+fn numbers_from_line(line: &str) -> Vec<char> {
+    let mut numbers = Vec::new();
+    line.chars().for_each(|char| {
+        if char.is_numeric() {
+            numbers.push(char)
+        }
+    });
+    return numbers;
+}
+
+fn main() {
+    let contents = fs::read_to_string("input.txt").expect("The file can't be read");
+    let lines: Vec<_> = contents
+        .split("\n")
+        .map(|msg| -> Vec<char> { return numbers_from_line(msg) })
+        .collect();
+
+    println!("lines: {:?}", lines);
 }
