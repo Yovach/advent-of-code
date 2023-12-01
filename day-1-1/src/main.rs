42b53aaf80496a5af2bd9bd076c9cc4de1555fd6
Quentin <contact@yovach.dev>
2023-12-01 10:39:52 +0100

    ref: improve readability of the code and add comments

--- day-1-1/src/main.rs
+++ day-1-1/src/main.rs
@@ -2,22 +2,21 @@ use core::panic;
 use std::fs;
 
 fn numbers_from_line(line: &str) -> Vec<u32> {
+    // Store all numbers from the line into a vec
     let mut numbers: Vec<u32> = Vec::new();
     line.chars().for_each(|char| {
         if char.is_numeric() {
             numbers.push(char.to_digit(10).unwrap())
         }
     });
+
     if numbers.len() == 0 {
         panic!("The line don't have any numbers");
     } else if numbers.len() == 1 {
         let single_number = numbers
             .get(0)
             .expect("We except to retrieve at least one number");
-        println!("char: {single_number}");
-        let calculation = single_number.clone() + single_number.clone();
-        println!("calculation {calculation}");
-        return [single_number.clone(), single_number.clone()].to_vec();
+        return vec![single_number.clone(), single_number.clone()];
     }
 
     let first_number = numbers
@@ -26,11 +25,11 @@ fn numbers_from_line(line: &str) -> Vec<u32> {
     let last_number = numbers
         .get(numbers.len() - 1)
         .expect("We except to retrieve the last number");
-
-    return [first_number.clone(), last_number.clone()].to_vec();
+    return vec![first_number.clone(), last_number.clone()];
 }
 
-fn calculation_from_numbers(values: Vec<u32>) -> u32 {
+// ([7,7] => 77, [8,5] => 85)
+fn concat_numbers(values: Vec<u32>) -> u32 {
     let first_number = values
         .get(0)
         .expect("We except to retrieve the first number");
@@ -46,12 +45,12 @@ fn main() {
     let calculations: Vec<_> = contents
         .split("\n")
         .map(|msg| -> Vec<u32> { return numbers_from_line(msg) })
-        .map(|msg| -> u32 { return calculation_from_numbers(msg) })
+        .map(|msg| -> u32 { return concat_numbers(msg) })
         .collect();
 
     let total = calculations
         .iter()
         .fold(0, |previous, current| -> u32 { return previous + current });
 
-    println!("lines: {:?}", total);
+    println!("total: {:?}", total);
 }
