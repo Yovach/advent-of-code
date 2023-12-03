5d6283d97d6f0f25e047f5ac2b48c3e592971fb4
Quentin <contact@yovach.dev>
2023-12-03 11:15:49 +0100

    get game_id from input

--- day-2/src/main.rs
+++ day-2/src/main.rs
@@ -1,5 +1,11 @@
 use std::fs;
 
+fn get_game_id(chunk: &str) -> u32 {
+    let parts: Vec<&str> = chunk.split(" ").collect();
+    let game_id = parts.get(1).expect("i expected a game id");
+    return game_id.parse::<u32>().unwrap();
+}
+
 fn main() {
     let contents = fs::read_to_string("input.txt").expect("The file can't be read");
     let games: Vec<&str> = contents.lines().collect();
@@ -9,8 +15,9 @@ fn main() {
         if data_chunks.len() != 2 {
             panic!("excepted a chunks with 2 elements but received : {:?}", data_chunks.len())
         }
+        let game_id = get_game_id(data_chunks.first().expect("a ID for the game is expected"));
         let cube_sets: Vec<&str> = game.split(";").collect();
-        println!("cube_sets: {:?}", cube_sets);
+        println!("cube_sets: {:?}", game_id);
     }
 
     println!("games : {:?}", games);
