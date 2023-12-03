b6eb42a30e280786bd4a7087d26efd051a3ae6ac
Quentin <contact@yovach.dev>
2023-12-03 11:54:46 +0100

    feat: add cube quantity calculation for color

--- day-2/src/main.rs
+++ day-2/src/main.rs
@@ -10,12 +10,13 @@ fn get_cube_chunks(chunk: &str) -> Vec<&str> {
     let cube_sets: Vec<&str> = chunk.trim().split(";").map(|cube| cube.trim()).collect();
     return cube_sets;
 }
-fn sum_cube_sets_count(cube_sets: &Vec<&str>) {
+fn sum_cube_sets_count(cube_sets: Vec<&str>) -> HashMap<&str, u32> {
     let mut store: HashMap<&str, u32> = HashMap::new();
 
     for cube_set in cube_sets {
         let cubes: Vec<&str> = cube_set.split(",").map(|cube| cube.trim()).collect();
 
+        println!("cube_set : {:?}", cube_set);
         for cube in cubes {
             let cubes_data: Vec<&str> = cube.split(" ").collect();
             let cube_nb = cubes_data
@@ -25,19 +26,20 @@ fn sum_cube_sets_count(cube_sets: &Vec<&str>) {
                 .unwrap();
             let cube_color = cubes_data.last().expect("expected a color");
 
-            if store.contains_key(cube_color) {
-                store
-                    .entry(cube_color)
-                    .and_modify(|value| *value += cube_nb);
-            } else {
-                store.entry(cube_color).or_insert(0);
-            }
+            println!("color {:?} with quantity {:?}", cube_color, cube_nb);
+            store
+                .entry(cube_color)
+                .and_modify(|value| *value += cube_nb)
+                .or_insert(cube_nb);
         }
     }
+
+    println!("store : {:?}", store);
+
     return store;
 }
 
-fn parse_game_data(game: &str) -> (u32, Vec<&str>) {
+fn parse_game_data(game: &str) -> (u32, Vec<&str>, HashMap<&str, u32>) {
     let data_chunks: Vec<&str> = game.split(":").collect();
     if data_chunks.len() != 2 {
         panic!(
@@ -48,8 +50,8 @@ fn parse_game_data(game: &str) -> (u32, Vec<&str>) {
     let game_id = get_game_id(data_chunks.first().expect("a ID for the game is expected"));
     let cube_sets = get_cube_chunks(data_chunks.last().expect("a chunks with cube sets"));
 
-    sum_cube_sets_count(&cube_sets);
-    return (game_id, cube_sets);
+    let cube_counts = sum_cube_sets_count(cube_sets.clone());
+    return (game_id, cube_sets, cube_counts);
 }
 
 fn main() {
@@ -57,7 +59,8 @@ fn main() {
     let games: Vec<&str> = contents.lines().collect();
 
     for game in &games {
-        let (game_id, cube_sets) = parse_game_data(game);
-        println!("game_id: {:?}, cube_sets: {:?}", game_id, cube_sets);
+        let (game_id, cube_sets, cube_counts) = parse_game_data(game);
+        println!("");
+        // println!("game_id: {:?}, cube_counts: {:?}", game_id, cube_counts);
     }
 }
