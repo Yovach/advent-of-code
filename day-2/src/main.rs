fac3dd3b25b167935029ccea45a8c62c9fe57e85
Quentin <contact@yovach.dev>
2023-12-03 11:18:57 +0100

    get cube sets from input

--- day-2/src/main.rs
+++ day-2/src/main.rs
@@ -6,6 +6,12 @@ fn get_game_id(chunk: &str) -> u32 {
     return game_id.parse::<u32>().unwrap();
 }
 
+fn get_cube_chunks(chunk: &str) -> Vec<&str> {
+    let cube_sets: Vec<&str> = chunk.trim().split(";").map(|cube| cube.trim()).collect();
+    println!("chunk : {:?}", chunk);
+    return cube_sets;
+}
+
 fn main() {
     let contents = fs::read_to_string("input.txt").expect("The file can't be read");
     let games: Vec<&str> = contents.lines().collect();
@@ -13,12 +19,14 @@ fn main() {
     for game in &games {
         let data_chunks: Vec<&str> = game.split(":").collect();
         if data_chunks.len() != 2 {
-            panic!("excepted a chunks with 2 elements but received : {:?}", data_chunks.len())
+            panic!(
+                "excepted a chunks with 2 elements but received : {:?}",
+                data_chunks.len()
+            )
         }
         let game_id = get_game_id(data_chunks.first().expect("a ID for the game is expected"));
-        let cube_sets: Vec<&str> = game.split(";").collect();
-        println!("cube_sets: {:?}", game_id);
+        let cube_sets = get_cube_chunks(data_chunks.last().expect("a chunks with cube sets"));
+        println!("cube_sets: {:?}", cube_sets);
     }
 
-    println!("games : {:?}", games);
 }
