e371713a4dcda90aa1b2950bf2b0798ceae3463b
Quentin <contact@yovach.dev>
2023-12-03 11:10:53 +0100

    check data chunks size before parsing game data

--- day-2/src/main.rs
+++ day-2/src/main.rs
@@ -4,5 +4,14 @@ fn main() {
     let contents = fs::read_to_string("input.txt").expect("The file can't be read");
     let games: Vec<&str> = contents.lines().collect();
 
+    for game in &games {
+        let data_chunks: Vec<&str> = game.split(":").collect();
+        if data_chunks.len() != 2 {
+            panic!("excepted a chunks with 2 elements but received : {:?}", data_chunks.len())
+        }
+        let cube_sets: Vec<&str> = game.split(";").collect();
+        println!("cube_sets: {:?}", cube_sets);
+    }
+
     println!("games : {:?}", games);
 }
