54eb4e0fca5e990a64eb5c7cf48a3d3e5fc05e1e
Quentin <contact@yovach.dev>
2023-12-03 11:37:14 +0100

    feat: store cubes nb for future sum calculation

--- day-2/src/main.rs
+++ day-2/src/main.rs
@@ -1,4 +1,4 @@
-use std::fs;
+use std::{collections::HashMap, fs};
 
 fn get_game_id(chunk: &str) -> u32 {
     let parts: Vec<&str> = chunk.split(" ").collect();
@@ -9,6 +9,32 @@ fn get_game_id(chunk: &str) -> u32 {
 fn get_cube_chunks(chunk: &str) -> Vec<&str> {
     let cube_sets: Vec<&str> = chunk.trim().split(";").map(|cube| cube.trim()).collect();
     return cube_sets;
+}
+fn sum_cube_sets_count(cube_sets: &Vec<&str>) {
+    let mut store: HashMap<&str, u32> = HashMap::new();
+
+    for cube_set in cube_sets {
+        let cubes: Vec<&str> = cube_set.split(",").map(|cube| cube.trim()).collect();
+
+        for cube in cubes {
+            let cubes_data: Vec<&str> = cube.split(" ").collect();
+            let cube_nb = cubes_data
+                .first()
+                .expect("expected a quantity")
+                .parse::<u32>()
+                .unwrap();
+            let cube_color = cubes_data.last().expect("expected a color");
+
+            if store.contains_key(cube_color) {
+                store
+                    .entry(cube_color)
+                    .and_modify(|value| *value += cube_nb);
+            } else {
+                store.entry(cube_color).or_insert(0);
+            }
+        }
+    }
+    return store;
 }
 
 fn parse_game_data(game: &str) -> (u32, Vec<&str>) {
@@ -21,6 +47,8 @@ fn parse_game_data(game: &str) -> (u32, Vec<&str>) {
     }
     let game_id = get_game_id(data_chunks.first().expect("a ID for the game is expected"));
     let cube_sets = get_cube_chunks(data_chunks.last().expect("a chunks with cube sets"));
+
+    sum_cube_sets_count(&cube_sets);
     return (game_id, cube_sets);
 }
 
