95319b4984acf2264eb7a811b04871f22feb9092
Quentin <contact@yovach.dev>
2023-12-04 18:04:56 +0100

    feat: get all numbers from grid

--- day-3/part-1/src/main.rs
+++ day-3/part-1/src/main.rs
@@ -1,4 +1,4 @@
-use std::{fs, str::Lines};
+use std::{collections::HashMap, fs, str::Lines};
 
 #[derive(Debug, Clone)]
 struct Position(usize, usize);
@@ -23,12 +23,58 @@ fn items_from_lines(lines: Lines<'_>) -> Vec<Cell> {
     return items;
 }
 
+fn get_numbers_with_positions(items: &Vec<Cell>) {
+    // let mut numbers_list: HashMap<Position, u32> = HashMap::new();
+    let mut numbers_list: Vec<String> = Vec::new();
+
+    let mut previous_cell: Option<&Cell> = Option::None;
+    let mut current_number = "".to_string();
+    for item in items {
+        if item.value.is_numeric() {
+            if previous_cell.is_none() {
+                current_number.push_str(item.value.to_string().as_str())
+            } else {
+                if previous_cell.unwrap().position.0 > item.position.0 {
+                    numbers_list.push(current_number.clone());
+                    current_number = "".to_string();
+                    continue;
+                } else if previous_cell.unwrap().position.0 == (item.position.0 - 1) {
+                    current_number.push_str(item.value.to_string().as_str())
+                }
+                // current_number.push_str(item.value.to_string().as_str());
+                // println!(
+                //     "previous: {:?}, current: {:?}, sum: {:?}",
+                //     previous_cell.unwrap().position.0,
+                //     item.position.0,
+                //     0 // (item.position.0 - previous_cell.unwrap().position.0)
+                // )
+                // println!(
+                //     "sum : {:?}, previous: {:?}, current: {:?}",
+                //     previous_cell.unwrap().position.0 == (item.position.0 - 1),
+                //     previous_cell,
+                //     item,
+                // )
+                // if previous_cell.unwrap().position.0 == (item.position.0 - 1) {
+                //     current_number.push_str(item.value.to_string().as_str())
+                // }
+            }
+        } else {
+            numbers_list.push(current_number.clone());
+            current_number = "".to_string();
+        }
+        previous_cell = Option::Some(item);
+    }
+
+    println!("number list : {:?}", numbers_list);
+}
+
 fn get_neighbours(items: &Vec<Cell>, target: &Cell, distance: usize) -> Vec<Cell> {
     let mut neighbours: Vec<Cell> = Vec::new();
     for item in items {
         let difference_x = item.position.0.abs_diff(target.position.0);
         let difference_y = item.position.1.abs_diff(target.position.1);
-        if difference_x <= distance && difference_y <= distance && (difference_x + difference_y > 0) {
+        if difference_x <= distance && difference_y <= distance && (difference_x + difference_y > 0)
+        {
             neighbours.push(item.clone());
         }
     }
@@ -44,6 +90,8 @@ fn parse_lines(lines: Lines<'_>) {
             println!("neighbours of {:?} : {:?}", index, neighbours);
         }
     }
+
+    get_numbers_with_positions(&items);
 
     // println!("numbers: {:?}", numbers);
 }
