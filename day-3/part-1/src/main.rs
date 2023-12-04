52c8d951addae63455e1eafef977b4b9a3623ca1
Quentin <contact@yovach.dev>
2023-12-04 15:03:47 +0100

    convert struct to tuple struct

--- day-3/part-1/src/main.rs
+++ day-3/part-1/src/main.rs
@@ -1,70 +1,32 @@
-use std::{fs, str::Lines, thread::current};
+use std::{fs, str::Lines};
 
 #[derive(Debug)]
-struct Position {
-    x: usize,
-    y: usize,
-}
+struct Position(usize, usize);
 
 #[derive(Debug)]
-struct Cell {
-    position: Position,
-    value: char,
-}
-
-#[derive(Debug)]
-struct CellNumber {
-    position: Position,
-    value: u32,
-}
+struct Cell(Position, char);
 
 fn items_from_lines(lines: Lines<'_>) -> Vec<Cell> {
     let mut items: Vec<Cell> = Vec::new();
     for (v_index, line) in lines.enumerate() {
         let line_values = line.trim().chars();
         for (h_index, item) in line_values.enumerate() {
-            items.push(Cell {
-                position: Position {
-                    x: h_index,
-                    y: v_index,
-                },
-                value: item,
-            })
+            items.push(Cell(
+                Position(h_index, v_index),
+                item,
+            ))
         }
     }
     return items;
 }
 
-fn get_neighbours(items: &Vec<Cell>) {
-
-}
-
-// fn parse_numbers_from_lines(items: Vec<&Cell>) {
-//     let mut cell_numbers: Vec<CellNumber> = Vec::new();
-//     let mut previous_cell: CellNumber;
-
-//     let mut current_number = String::from("");
-
-//     println!("numbers: {:?}", items);
-//     for item in items {
-
-//         current_number.push_str(&item.value.to_string());
-
-//         previous_cell = CellNumber {
-//             value  
-//         }
-
-//         cell_numbers.push(CellNumber {
-//             value: 5,
-//         })
-//     }
-// }
+fn get_neighbours(items: &Vec<Cell>) {}
 
 fn parse_lines(lines: Lines<'_>) {
     let items = items_from_lines(lines);
     for (index, item) in items.iter().enumerate() {
-        if item.value.is_numeric() {
-
+        if item.1.is_numeric() {
+            println!("item: {:?}", item.1);
         }
     }
 
