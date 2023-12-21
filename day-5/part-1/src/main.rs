19d2c433c00dc64cdf92396f5fedf77f93c3fc0d
Quentin <contact@yovach.dev>
2023-12-21 14:24:38 +0100

    for on range seed/soil

--- day-5/part-1/src/main.rs
+++ day-5/part-1/src/main.rs
@@ -1,18 +1,35 @@
-use std::fs;
+use std::{fs, ops::Add};
 
 fn main() {
-    let file_content = fs::read_to_string("./input.txt").expect("i should be able to read this file");
+    let file_content =
+        fs::read_to_string("./input.txt").expect("i should be able to read this file");
     let lines: Vec<&str> = file_content.lines().collect();
 
     for line in &lines {
         let elements: Vec<&str> = line.split(" ").collect();
         if elements.len() == 3 {
-            let destination = elements.get(0).expect("i expected a `destination` value").parse::<u32>().expect("i expected a u32 for `destination` value");
-            let source = elements.get(1).expect("i expected a `source` value").parse::<u32>().expect("i expected a u32 for `source` value");
-            let range = elements.get(2).expect("i expected a `range` value").parse::<u32>().expect("i expected a u32 for `range` value");
+            let destination = elements
+                .get(0)
+                .expect("i expected a `destination` value")
+                .parse::<u32>()
+                .expect("i expected a u32 for `destination` value");
+            let source = elements
+                .get(1)
+                .expect("i expected a `source` value")
+                .parse::<u32>()
+                .expect("i expected a u32 for `source` value");
+            let range = elements
+                .get(2)
+                .expect("i expected a `range` value")
+                .parse::<u32>()
+                .expect("i expected a u32 for `range` value");
             println!("{destination} {source} {range}");
-        
-                }
+
+            for i in 0..range {
+                println!("seed: {:?} => soil {:?}", source.add(i), destination.add(i));
+                // println!("range: {:?}", i);
+            }
+        }
     }
 
     // println!("{:?}", lines);
