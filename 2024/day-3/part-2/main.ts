e4468af3be7d1347b9cdbc3bedc079c1d2af5959
Quentin <contact@yovach.dev>
2024-12-03 21:34:21 +0100

    handle don't, do and mul instructions

--- 2024/day-3/part-2/main.ts
+++ 2024/day-3/part-2/main.ts
@@ -1,19 +1,39 @@
+function parseAsInt(val: string) {
+  return parseInt(val, 10);
+}
+
+type Instruction = {
+  method: string;
+  arguments: string;
+}
+
 const fileContent: string = Deno.readTextFileSync("./input.txt").trimEnd();
 
-const handledInstructions: string[] = [];
 const regexInstructions = /([a-z]+'?[a-z]+)\((.*?)\)/g;
+
+let canExecuteNext = true;
+
+let total = 0;
+
 for (const match of fileContent.matchAll(regexInstructions)) {
-  const instruction = match[1].trim();
-  if (!handledInstructions.includes(instruction)) {
-    handledInstructions.push(instruction);
+  const instructionMethod = match[1];
+  console.log(instructionMethod)
+
+  if (instructionMethod === "mul") {
+    if (canExecuteNext === false) {
+      continue;
+    }
+
+    const instructionArguments = match[2];
+    const args = instructionArguments.split(",").map(parseAsInt);
+    if (args.length === 2 && args.every((value) => !Number.isNaN(value))) {
+      total += args[0] * args[1]
+    }
+  } else if (instructionMethod === "do") {
+    canExecuteNext = true;
+  } else if (instructionMethod === "don't") {
+    canExecuteNext = false;
   }
 }
 
-// let total = 0;
-// for (const match of fileContent.matchAll(regex)) {
-//   const firstNum = parseAsInt(match[1]);
-//   const secondNum = parseAsInt(match[2]);
-//   total += firstNum * secondNum;
-
-// }
-// console.log(total)
+console.log(total)
