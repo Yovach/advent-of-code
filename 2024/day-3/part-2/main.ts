0a46ab8a51261f8c5b495ce685ddf0007df84c88
Quentin <contact@yovach.dev>
2024-12-03 22:30:53 +0100

    perf: optimize

--- 2024/day-3/part-2/main.ts
+++ 2024/day-3/part-2/main.ts
@@ -2,36 +2,28 @@ function parseAsInt(val: string) {
   return parseInt(val, 10);
 }
 
-type Instruction = {
-  method: string;
-  arguments: string;
-}
-
 const fileContent: string = Deno.readTextFileSync("./input.txt").trimEnd();
+const regexInstructions: RegExp = /(do|don't)\(\)|(mul)\((\d+),(\d+)\)/g;
 
-const handledInstructions = ["(do)\\(\\)", "(don't)\\(\\)", "(mul)\\((\\d+),(\\d+)\\)"]
-
-const regexInstructions = new RegExp("(" + handledInstructions.join("|") + ")", "g")
 let canExecuteNext = true;
-
 let total = 0;
 
 for (const match of fileContent.matchAll(regexInstructions)) {
-  const instructionMethod = match[1];
-  if (instructionMethod.startsWith("mul")) {
+  const instructionMethod = match[1] ?? match[2];
+  if (instructionMethod === "mul") {
     if (canExecuteNext === false) {
       continue;
     }
 
-    const firstNumber = parseAsInt(match[5]);
-    const secondNumber = parseAsInt(match[6]);
+    const firstNumber = parseAsInt(match[3]);
+    const secondNumber = parseAsInt(match[4]);
     if (!Number.isNaN(firstNumber) && !Number.isNaN(secondNumber)) {
       total += firstNumber * secondNumber;
     }
-  } else if (instructionMethod.startsWith("don't")) {
-    canExecuteNext = false;
-  } else if (instructionMethod.startsWith("do")) {
+  } else if (instructionMethod === "do") {
     canExecuteNext = true;
+  } else if (instructionMethod === "don't") {
+    canExecuteNext = false;
   }
 }
 
