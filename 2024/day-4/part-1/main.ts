733e932930c0139577db377f43fdd571a2a6cd1e
Quentin <contact@yovach.dev>
2024-12-04 20:48:26 +0100

    add checks for diagonal

--- 2024/day-4/part-1/main.ts
+++ 2024/day-4/part-1/main.ts
@@ -6,7 +6,6 @@ assert(import.meta.dirname);
 const filePath = joinPath(import.meta.dirname, "..", "input.txt");
 const fileContent: string = Deno.readTextFileSync(filePath).trimEnd();
 
-const directions = [-1, 0, 1];
 let nbOccurrences = 0;
 
 const lines = fileContent.split("\n");
@@ -26,35 +25,57 @@ for (
     const character = chars[horizontalIdx];
 
     if (character === "X") {
-      console.log(lines[verticalIdx][horizontalIdx])
-      if ( // <----
-        chars[horizontalIdx - 1] === "M" &&
-        chars[horizontalIdx - 2] === "A" &&
-        chars[horizontalIdx - 3] === "S"
-      ) {
-        nbOccurrences++;
-      } else if ( // ---->
-        chars[horizontalIdx + 1] === "M" &&
-        chars[horizontalIdx + 2] === "A" &&
-        chars[horizontalIdx + 3] === "S"
-      ) {
-        nbOccurrences++;
-      } else if ( // /\
-        lines[verticalIdx - 1]?.[horizontalIdx] === "M" &&
-        lines[verticalIdx - 2]?.[horizontalIdx] === "A" &&
-        lines[verticalIdx - 3]?.[horizontalIdx] === "S"
-      ) {
-        nbOccurrences++;
-      } else if ( // \/
-        lines[verticalIdx + 1]?.[horizontalIdx] === "M" &&
-        lines[verticalIdx + 2]?.[horizontalIdx] === "A" &&
-        lines[verticalIdx + 3]?.[horizontalIdx] === "S"
+      console.log(lines[verticalIdx][horizontalIdx]);
+      if (
+        // Horizontal
+        (
+          chars[horizontalIdx - 1] === "M" &&
+          chars[horizontalIdx - 2] === "A" &&
+          chars[horizontalIdx - 3] === "S"
+        ) || (
+          chars[horizontalIdx + 1] === "M" &&
+          chars[horizontalIdx + 2] === "A" &&
+          chars[horizontalIdx + 3] === "S"
+        )
       ) {
         nbOccurrences++;
       } else if (
-        lines[verticalIdx + 1]?.[horizontalIdx] === "M" &&
-        lines[verticalIdx + 2]?.[horizontalIdx] === "A" &&
-        lines[verticalIdx + 3]?.[horizontalIdx] === "S"
+        // Vertical
+        (
+          lines[verticalIdx - 1]?.[horizontalIdx] === "M" &&
+          lines[verticalIdx - 2]?.[horizontalIdx] === "A" &&
+          lines[verticalIdx - 3]?.[horizontalIdx] === "S"
+        ) || (
+          lines[verticalIdx + 1]?.[horizontalIdx] === "M" &&
+          lines[verticalIdx + 2]?.[horizontalIdx] === "A" &&
+          lines[verticalIdx + 3]?.[horizontalIdx] === "S"
+        )
+      ) {
+        nbOccurrences++;
+      } else if (
+        // Diagonal \
+        (
+          lines[verticalIdx - 1]?.[horizontalIdx - 1] === "M" &&
+          lines[verticalIdx - 2]?.[horizontalIdx - 2] === "A" &&
+          lines[verticalIdx - 3]?.[horizontalIdx - 3] === "S"
+        ) || (
+          lines[verticalIdx + 1]?.[horizontalIdx + 1] === "M" &&
+          lines[verticalIdx + 2]?.[horizontalIdx + 2] === "A" &&
+          lines[verticalIdx + 3]?.[horizontalIdx + 3] === "S"
+        )
+      ) {
+        nbOccurrences++;
+      } else if (
+        // Diagonal /
+        (
+          lines[verticalIdx - 1]?.[horizontalIdx + 1] === "M" &&
+          lines[verticalIdx - 2]?.[horizontalIdx + 2] === "A" &&
+          lines[verticalIdx - 3]?.[horizontalIdx + 3] === "S"
+        ) || (
+          lines[verticalIdx + 1]?.[horizontalIdx - 1] === "M" &&
+          lines[verticalIdx + 2]?.[horizontalIdx - 2] === "A" &&
+          lines[verticalIdx + 3]?.[horizontalIdx - 3] === "S"
+        )
       ) {
         nbOccurrences++;
       }
