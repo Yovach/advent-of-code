1f328bd8ccea516b0cd3674584d6fb74ff5cce80
Quentin <contact@yovach.dev>
2024-12-04 12:56:51 +0000

    check for SAMX and XMAS (horizontally)

--- 2024/day-4/part-1/main.ts
+++ 2024/day-4/part-1/main.ts
@@ -5,3 +5,43 @@ assert(import.meta.dirname);
 
 const filePath = joinPath(import.meta.dirname, "..", "input.txt");
 const fileContent: string = Deno.readTextFileSync(filePath).trimEnd();
+
+const directions = [-1, 0, 1];
+let nbOccurrences = 0;
+
+const lines = fileContent.split("\n");
+for (
+  let verticalIdx = 0, nbLines = lines.length;
+  verticalIdx < nbLines;
+  verticalIdx++
+) {
+  const line = lines[verticalIdx];
+
+  const characters = line.split("");
+
+  for (
+    let horizontalIdx = 0, nbChars = characters.length;
+    horizontalIdx < nbChars;
+    horizontalIdx++
+  ) {
+    const character = characters[horizontalIdx];
+
+    if (character === "X") {
+      if (
+        characters[horizontalIdx - 1] === "M" &&
+        characters[horizontalIdx - 2] === "A" &&
+        characters[horizontalIdx - 3] === "S"
+      ) {
+        nbOccurrences++;
+      } else if (
+        characters[horizontalIdx + 1] === "M" &&
+        characters[horizontalIdx + 2] === "A" &&
+        characters[horizontalIdx + 3] === "S"
+      ) {
+        nbOccurrences++;
+      }
+    }
+  }
+}
+
+console.log({ nbOccurrences });
