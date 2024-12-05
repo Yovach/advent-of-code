320b6b459f28e22d44c193e6f4b3bbfc31027ce0
Quentin <contact@yovach.dev>
2024-12-05 12:53:44 +0000

    prepare day 5

---
+++ 2024/day-5/part-1/main.ts
@@ -0,0 +1,9 @@
+import { join as joinPath } from "@std/path";
+import { assert } from "@std/assert";
+
+assert(import.meta.dirname);
+
+const filePath = joinPath(import.meta.dirname, "..", "input.txt");
+const lines: string[] = Deno
+  .readTextFileSync(filePath)
+  .trimEnd();
