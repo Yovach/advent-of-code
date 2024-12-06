741c8052fa308a7092db6b9ef4fd91936d892972
Quentin <contact@yovach.dev>
2024-12-06 12:22:57 +0000

    exclude empty lines

--- 2024/day-5/part-1/main.ts
+++ 2024/day-5/part-1/main.ts
@@ -6,4 +6,14 @@ assert(import.meta.dirname);
 const filePath = joinPath(import.meta.dirname, "..", "input.txt");
 const lines: string[] = Deno
   .readTextFileSync(filePath)
-  .trimEnd();
+  .trimEnd()
+  .split("\n").filter((line) => line.length > 0);
+
+for (const line of lines) {
+  if (line.includes("|")) {
+    // Page ordering rules
+    console.log(line)
+  } else if (line.includes(",")) {
+
+  }
+}
