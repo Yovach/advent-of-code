a78c32dc003a021dd7721da028f58122e8ee5b63
Quentin <contact@yovach.dev>
2024-12-06 12:28:48 +0000

    day 5 part 1 [wip]

--- 2024/day-5/part-1/main.ts
+++ 2024/day-5/part-1/main.ts
@@ -1,7 +1,16 @@
 import { join as joinPath } from "@std/path";
 import { assert } from "@std/assert";
 
+function parseAsInt(val: string) {
+  return parseInt(val, 10);
+}
+
 assert(import.meta.dirname);
+
+type PageOrderingRule = {
+  before: number;
+  after: number;
+};
 
 const filePath = joinPath(import.meta.dirname, "..", "input.txt");
 const lines: string[] = Deno
@@ -9,11 +18,23 @@ const lines: string[] = Deno
   .trimEnd()
   .split("\n").filter((line) => line.length > 0);
 
+const pageOrderingRules: PageOrderingRule[] = [];
+
+function areUpdatesRightOrder(updates: number[]) {
+  for (const update of updates) {
+  }
+}
+
 for (const line of lines) {
   if (line.includes("|")) {
     // Page ordering rules
-    console.log(line)
-  } else if (line.includes(",")) {
+    console.log(line);
+    const [before, after] = line.split("|").map(parseAsInt);
+    pageOrderingRules.push({ before, after });
 
+    console.log(`before: ${before}, after: ${after}`);
+  } else if (line.includes(",")) {
+    const updates = line.split(",").map(parseAsInt);
+    console.log({ updates });
   }
 }
